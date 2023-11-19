499dc587a7ee766fefa6aca665e216a4597a4b83
matt <harry.little@gmx.com>
2023-11-19 15:25:32 +0100

    increase numeric value

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Lock {
-    uint256 valNum = 699;
+    uint256 valNum = 6999;
     uint public unlockTime;
     address payable public owner;
 
